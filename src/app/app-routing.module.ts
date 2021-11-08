a34ca7cebe9e7af936fa6033c0a4e2f33876c1a1
Mattis Erkensten <m.erkensten@icloud.com>
2021-11-08 15:56:24 +0100

    Routing fixat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ToDoPageComponent } from './pages/to-do-page/to-do-page.component';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'to-do', component: ToDoPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
