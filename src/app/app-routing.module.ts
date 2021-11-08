db26db511a4ebc7baa78fbf599abab4d85c7a378
Mattis Erkensten <m.erkensten@icloud.com>
2021-11-08 16:06:27 +0100

    404 sida skapad & simpel header fixad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ToDoPageComponent } from './pages/to-do-page/to-do-page.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'to-do', component: ToDoPageComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ToDoPageComponent } from './pages/to-do-page/to-do-page.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, HomePageComponent, ToDoPageComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomePageComponent, ToDoPageComponent, PageNotFoundComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
